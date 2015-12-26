19e2f872e7a2126ba043f6d578d5725ffe9b8396
Daniel Stoinov <daniel.stoinov@gmail.com>
2015-12-26 10:58:42 -0500

    changed unarchive script to get arguments instead of hardcoded paths

--- files/unarchive-tarballs.sh
+++ files/unarchive-tarballs.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 
 # VARs
-dest_dir="/mnt/lfs/sources/"
-src_dir="/mnt/lfs/sources"
+#dest_dir="/mnt/lfs/sources/"
+#src_dir="/mnt/lfs/sources"
+
+# Get paths from the argument list
+
+dest_dir=$1
+src_dir=$2
+
+## check if direcotries exist
+
+if [ ! -d $1 ]; then
+  echo "The directory $1 doesn't exist"
+  exit 2
+fi
+
+if [ ! -d $2 ]; then
+  echo "The directory $2 doesn't exist"
+  exit 2
+fi
+
 
 # CMDs
 cmd_tar="/usr/bin/tar"
